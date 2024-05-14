f717ad70b4915255a429f60540f3ab460685001f
Yngauxide <danieloxide4all@gmail.com>
2024-05-14 01:00:42 +0100

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ function App() {
 <Nav />
     <Routes>
     <Route path="/" element={<Home />} />
-          {/* <Route path="/about" element={<About />} />
+          <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/contact" element={<Contact />} /> 
 
     </Routes>
     <Footer />
